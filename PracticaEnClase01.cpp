58d47c87d9277a00ae39d48b773fa42e18f8d7ea
Jorge Arturo V.M. <jorgeavm@una.ac.cr>
2025-08-25 21:14:51 -0600

    Práctica completada

--- PracticaEnClase01.cpp
+++ PracticaEnClase01.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <sstream>
-#include <windows.h> // Para SetConsoleOutputCP
-#include <cstdlib> // For rand() and srand()
-#include <ctime>   // For time()
+#include <windows.h> // Needed for: SetConsoleOutputCP
+#include <cstdlib> // Needed for: rand() and srand()
+#include <ctime>   // Needed for: time()
 #include <conio.h>
+#include <iomanip> //Needed for: setprecision
 using namespace std;
 
-void selectMenuOption(int selection);
+int** createMatrix(int* pStudents, int* pGrades);
+float calculateStudentAverage(int** pMatrix, int aStudent, int aGrades);
 void printMatrix(int** pMatrix, int aRows, int aCols);
+void calculateAllStudentsAverage(float* aStudentsAverage, int** pMatrix, int aStudents, int aGrades);
+void showAllStudentsAverage(float* sStudentsAverage, int** pMatrix, int aStudents);
+void calculateBestStudentsAverage(float* aBestStudentsAverage, float* aStudentsAverage);
+void showBestStudentsAverage(float* sBestStudentsAverage);
+void deleteMatrix(int** aGradesMatrix, int aStudent);
 
 int main()
 {
@@ -15,28 +22,13 @@ int main()
   srand(time(0)); // Seed the random number generator using the current time
   
   int students = 0, grades = 0;
-  while (students > 30 || students < 10) {
-    cout << "Digite la cantidad de estudiantes (mínimo 10, máximo 30): ";
-    cin >> students;
-  }
-  while (grades > 10 || grades < 5) {
-    cout << "Digite la cantidad de evaluaciones (mínimo 5, máximo 10): ";
-    cin >> grades;
-  }
-  grades++; //one for IDs
- 
-  int** gradesMatrix;
-  gradesMatrix = new int* [students];
-  for (int i = 0; i < students; i++) {
-    gradesMatrix[i] = new int[grades];
-  }
+  int** gradesMatrix = createMatrix(&students, &grades);
 
-  for (int rows = 0; rows < students; rows++) {
-    gradesMatrix[rows][0] = 1000 + rand() % 8999;
-    for (int cols = 1; cols < grades; cols++) {
-      gradesMatrix[rows][cols] = (rand() % 21) * 5;
-    }
-  }
+  float* studentAverages = new float[students];
+  bool isAveragesSet = false;
+
+  float bestStudentAverages[5];
+  bool isBestAveragesSet = false;
 
   int menuOption = -1;
   while (menuOption != 0)
@@ -44,9 +36,12 @@ int main()
     system("cls");
     cout << "Seleccione una opción escribiendo el número correspondiente:" << endl;
     cout << "0 -> Salir del programa." << endl;
-    cout << "1 -> Mostrar Matriz." << endl;
-    cout << "2 -> Mostrar Promedios de Notas de Estudiantes." << endl;
-    cout << "3 -> Mostrar 5 Promedios más altos." << endl;
+    cout << "1 -> Mostrar Matriz de Evaluaciones." << endl;
+    cout << "2 -> Generar Promedios de Notas de Estudiantes." << endl;
+    cout << "3 -> Mostrar Promedios de Notas de Estudiantes." << endl;
+    cout << "4 -> Calcular 5 Promedios más altos." << endl;
+    cout << "5 -> Mostrar 5 Promedios más altos." << endl;
+    cout << "6 -> Recrear Matriz de Evaluaciones." << endl;
     cin >> menuOption;
     if (menuOption == 1)
     {
@@ -54,21 +49,86 @@ int main()
     }
     else if (menuOption == 2)
     {
-
+      calculateAllStudentsAverage(studentAverages, gradesMatrix, students, grades);
+      isAveragesSet = true;
     }
     else if (menuOption == 3)
     {
-
+      if (isAveragesSet) {
+        showAllStudentsAverage(studentAverages, gradesMatrix, students);
+      }
+      else
+      {
+        cout << "Los promedios aún no han sido calculados." << endl;
+        cout << "Presione una tecla para regresar al menú" << endl;
+        _getch();
+      }
     }
     else if (menuOption == 4)
     {
-
+      if (isAveragesSet)
+      {
+        calculateBestStudentsAverage(bestStudentAverages, studentAverages);
+        isBestAveragesSet = true;
+      }
+      else
+      {
+        cout << "Los promedios aún no han sido calculados." << endl;
+        cout << "Presione una tecla para regresar al menú" << endl;
+        _getch();
+      }
+    }
+    else if (menuOption == 5)
+    {
+      if (isBestAveragesSet) {
+        showBestStudentsAverage(bestStudentAverages);
+      }
+      else
+      {
+        cout << "Los cinco mejores promedios aún no han sido calculados." << endl;
+        cout << "Presione una tecla para regresar al menú" << endl;
+        _getch();
+      }
+    }
+    else if (menuOption == 6)
+    {
+      deleteMatrix(gradesMatrix, students);
+      students = grades = 0;
+      gradesMatrix = createMatrix(&students, &grades);
+      isAveragesSet = isBestAveragesSet = false;
     }
   }
   return 0;
 }
 
-void printMatrix(int** pMatrix,int aRows,int aCols) {
+int** createMatrix(int* pStudents, int* pGrades)
+{
+  while (*pStudents > 30 || *pStudents < 10) {
+    cout << "Digite la cantidad de estudiantes (mínimo 10, máximo 30): ";
+    cin >> *pStudents;
+  }
+  while (*pGrades > 10 || *pGrades < 5) {
+    cout << "Digite la cantidad de evaluaciones (mínimo 5, máximo 10): ";
+    cin >> *pGrades;
+  }
+  *pGrades+=1; //adds one column for IDs
+
+  int** aGradesMatrix = new int* [*pStudents];
+  for (int i = 0; i < *pStudents; i++) {
+    aGradesMatrix[i] = new int[*pGrades];
+  }
+
+  for (int rows = 0; rows < *pStudents; rows++) {
+    aGradesMatrix[rows][0] = 1000 + rand() % 8999;
+    for (int cols = 1; cols < *pGrades; cols++) {
+      aGradesMatrix[rows][cols] = (rand() % 21) * 5;
+    }
+  }
+  return aGradesMatrix;
+}
+
+void printMatrix(int** pMatrix,int aRows,int aCols)
+{
   cout << "Carné\t";
 
   for (int cols = 1; cols < aCols; cols++)
@@ -86,4 +146,75 @@ void printMatrix(int** pMatrix,int aRows,int aCols) {
   }
   cout << "Presione una tecla para regresar al menú" << endl;
   _getch();
+}
+
+float calculateStudentAverage(int** pMatrix, int aStudent, int aGrades)
+{
+  float studentAverage = 0.0f;
+  for (int grades = 1; grades < aGrades; grades++)
+  {
+    studentAverage += pMatrix[aStudent][grades];
+  }
+  return studentAverage / (aGrades - 1);
+}
+
+void calculateAllStudentsAverage(float* aStudentsAverage, int** pMatrix, int aStudents, int aGrades)
+{
+  for (int students = 0; students < aStudents; students++)
+  {
+    aStudentsAverage[students] = calculateStudentAverage(pMatrix, students, aGrades);
+  }
+
+  int swapTemp;
+  for (int i = 0; i < aStudents - 1; ++i) {
+    for (int j = 0; j < aStudents - 1 - i; ++j) {
+      if (aStudentsAverage[j] < aStudentsAverage[j + 1]) {
+        swapTemp = aStudentsAverage[j];
+        aStudentsAverage[j] = aStudentsAverage[j + 1];
+        aStudentsAverage[j + 1] = swapTemp;
+      }
+    }
+  }
+
+  cout << "Promedios generados.\nPresione una tecla para regresar al menú" << endl;
+  _getch();
+}
+
+void showAllStudentsAverage(float* aStudentsAverage, int** pMatrix, int aStudents)
+{
+  for (int students = 0; students < aStudents; students++)
+  {
+    cout << "ID Estudiante: " << pMatrix[students][0];
+    cout << " Promedio: " << fixed << setprecision(2) << aStudentsAverage[students] << endl;
+  }
+  cout << "Presione una tecla para regresar al menú" << endl;
+  _getch();
+}
+void calculateBestStudentsAverage(float* aBestStudentsAverage, float* aStudentsAverage)
+{
+  for (int student = 0; student < 5; student++)
+  {
+    aBestStudentsAverage[student] = aStudentsAverage[student];
+  }
+  cout << "Cinco mejores promedios generados.\nPresione una tecla para regresar al menú" << endl;
+  _getch();
+}
+
+void showBestStudentsAverage(float* sBestStudentsAverage)
+{
+  cout << "Cinco mejores promedios" << endl;
+  for (int student = 0; student < 5; student++)
+  {
+    cout << fixed << setprecision(2) << sBestStudentsAverage[student] << endl;
+  }
+  cout << "Presione una tecla para regresar al menú" << endl;
+  _getch();
+}
+void deleteMatrix(int** aGradesMatrix, int aStudent)
+{
+  for (int rows = 0; rows < aStudent; rows++) {
+    delete[] aGradesMatrix[rows];
+  }
+  delete[] aGradesMatrix;
+  aGradesMatrix = NULL;
 }
